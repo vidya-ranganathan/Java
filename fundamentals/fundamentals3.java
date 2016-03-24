11995b187e3d06147f2f0258bf00e27066d1a486
vidya-ranganathan <vidya-ranganathan@users.noreply.github.com>
2016-03-24 16:13:35 +0530

    operations on variables

---
+++ fundamentals/fundamentals3.java
@@ -0,0 +1,13 @@
+public class fundamentals3 {
+
+     public static void main(String []args){
+         int i = 90;
+         short s = 2;
+         float ff = 10.99f;
+
+         System.out.println("integer is "+ff/s);
+         System.out.println("short is "+ff/s);
+         System.out.println("float is "+i/s);
+
+     }
+}
