d5d25f7fee5c499dcc5790f6d24f3a4bf17a583c
vidya-ranganathan <vidya-ranganathan@users.noreply.github.com>
2016-03-24 16:04:55 +0530

    demonstrate variable types

---
+++ fundamentals/Second.java
@@ -0,0 +1,13 @@
+public class Second{
+
+     public static void main(String []args){
+         int i = 90;
+         short s = 2;
+         float ff = 10.99f;
+         String name = "vidya";
+         System.out.println("integer is "+i);
+         System.out.println("short is "+s);
+         System.out.println("float is "+ff);
+         System.out.println("string is "+name);
+     }
+}
